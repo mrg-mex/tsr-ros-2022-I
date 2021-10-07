436c3e76ed433535de6ff1413182265d874528d2
Felipe Rivas <rivascf@gmail.com>
2021-10-07 17:51:09 -0500

    Programa publicador simple

---
+++ pubsub/src/publisher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import rospy
+from pubsub.msg import Estatus
+
+def publisher():
+    pub = rospy.Publisher('pubtopic', Estatus, queue_size=10)
+    rospy.init_node('pubnode', anonymous=False)  
+    r = rospy.Rate(10)  
+    msg = Estatus()
+    msg.estado = 'OK'
+    msg.codigo = 1000
+
+    while not rospy.is_shutdown():
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        r.sleep()
+
+if __name__ == "__main__":
+    publisher()
